c98607968294ad63e4de9f79a73011529129631e
JadynM <jadynmiller04@gmail.com>
2018-07-25 18:48:39 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,5 +1,11 @@
 #write out your code here
 
 def least_coins(cents)
+  solution = {}
+  
+#i need to take coins and figure out first how many quarters I have...
+
+num_quarters = cents / 25 
+solution[:quarters]=num_quarters
   
 end
