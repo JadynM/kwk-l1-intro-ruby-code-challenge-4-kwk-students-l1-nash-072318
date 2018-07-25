81afd90d35254f22859d864dff7c873cf331bf91
JadynM <jadynmiller04@gmail.com>
2018-07-25 18:33:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,6 @@
 #write out your code here
 
 def least_coins(cents)
-  puts least_coins == {":quarters" => 1, ":dimes" => 0, ":pennies" => 4}
+  puts least_coins == [":quarters" => 1, ":dimes" => 0, ":pennies" => 4]
 end
 least_coins
