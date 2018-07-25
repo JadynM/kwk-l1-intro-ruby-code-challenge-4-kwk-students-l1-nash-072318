3474a97ce302ae61606a47b1ae6f32e8cab6bcfa
JadynM <jadynmiller04@gmail.com>
2018-07-25 18:55:47 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -7,9 +7,18 @@ def least_coins(cents)
 
 num_quarters = cents / 25 
 solution[:quarters]=num_quarters
+cents = cents - 25*num_quarters
+
+num_dimes = cents / 10 
+solution[:dimes]=num_dimes
+cents = cents - 10*num_dimes
+
+num_nickles = cents / 5 
+solution[:]
+
   
 puts solution
 
 end
 
-least_coins(50)
+least_coins(70)
